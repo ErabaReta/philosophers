acb526a40dfa952f6c812600760fb128cd27e7c7
ErabaReta <mokhtarouhrich@gmail.com>
2024-05-17 20:29:07 +0100

    orginize philosophers to eat only in specefic time so they can all survive

--- exiter.c
+++ exiter.c
@@ -1,19 +1,19 @@
 #include "philo.h"
 
-void	exiter(int err)
+void	exiter(int err, t_philo *philo)
 {
 	int	i;
 
 	printf("exiting ..\n");
-	pthread_mutex_lock(&(get_vars()->state_lock));
+	pthread_mutex_lock(&(philo->vars->state_lock));
 	i = 0;
-	while ( i < get_vars()->number_of_philosophers)
+	while ( i < philo->vars->number_of_philosophers)
 	{
-		pthread_mutex_destroy(&(get_vars()->forks[i]));
+		pthread_mutex_destroy(&(philo->vars->forks[i]));
 		i++;
 	}
-	pthread_mutex_destroy(&(get_vars()->time_lock));
-	pthread_mutex_unlock(&(get_vars()->state_lock));
-	pthread_mutex_destroy(&(get_vars()->state_lock));
+	pthread_mutex_destroy(&(philo->vars->time_lock));
+	pthread_mutex_unlock(&(philo->vars->state_lock));
+	pthread_mutex_destroy(&(philo->vars->state_lock));
 	exit(err);
 }
